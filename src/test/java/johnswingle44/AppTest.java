7c494fa4d095ccc4ee14c6655c69c9e579cf72c3
Arthur Loder <arthur_loder@cable.comcast.com>
2017-03-25 04:17:39 -0400

    Initial commit

---
+++ src/test/java/johnswingle44/AppTest.java
@@ -0,0 +1,13 @@
+package johnswingle44;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        Assert.assertTrue(true);
+    }
+
+}
